587f885a41eafb539e583244bfd6ed94a60b061b
lilasorann <carrentals32ew@gmail.com>
2025-04-11 17:56:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "255697608274"
+global.ownernomer = "2347046559453"
 global.ownername = "𝐷𝛥𝛮𝛮𝑌"
-global.ownernumber = '255697608274'  //owner number 2
+global.ownernumber = '2347046559453'  //owner number 2
 //put your phone number above
 //let the owner as Danny or you can just put your name
 
@@ -18,14 +18,14 @@ global.prefa = [',','!','.','#','&']
 
 //false=disable and true=enable
 global.autoRecording = false //auto recording
-global.autoTyping = true //auto typing
+global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = true //auto update bio
 global.anti91 = false //auto block +91 
-global.autoswview = true //auto view status/story
+global.autoswview = false //auto view status/story
 global.antilink = true; // Default: disabled
-global.autostatusReact = true; // Default: disabled
+global.autostatusReact = false; // Default: disabled
 global.reactEmoji = '😂'; // Default reaction emoji
 //Thank you for using 𝓒𝓡𝓔𝓔𝓟𝓨_𝓜𝓓-𝓥𝟏
 //suppoet us by following our official channel
